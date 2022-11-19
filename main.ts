35374e9d1950346f5935411d96fa3fa46afd41a0
EccentricVamp <EccentricVamp@gmail.com>
2022-11-19 16:52:18 -0600

    Fix hydration again

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { getSheet } from "twind";
+import { dom } from "twind";
 import { FreshwindUserConfig, setup, STYLE_ELEMENT_ID } from "./shared.ts";
 
 export default function hydrate(options: FreshwindUserConfig) {
-  const sheet = getSheet(true, false);
-  // @ts-ignore Remove this once Twind plays nice with islands
-  sheet.target = document.getElementById(STYLE_ELEMENT_ID) as HTMLStyleElement;
+  const sheet = dom(
+    document.getElementById(STYLE_ELEMENT_ID) as HTMLStyleElement,
+  );
   setup(options, sheet);
 }
